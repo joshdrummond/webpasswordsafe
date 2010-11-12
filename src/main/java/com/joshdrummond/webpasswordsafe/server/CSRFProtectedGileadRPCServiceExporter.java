bc17df8e7facbdfcf706e06a318b8609082c42cc
Josh Drummond <joshdrummond@users.noreply.github.com>
2010-11-12 00:45:53 +0000

    Added CSRF/XSRF protection, updated GWT dependency to 2.1.0 and GXT dependency to 2.2.1

---
+++ src/main/java/com/joshdrummond/webpasswordsafe/server/CSRFProtectedGileadRPCServiceExporter.java
@@ -0,0 +1,42 @@
+/*
+    Copyright 2010 Josh Drummond
+
+    This file is part of WebPasswordSafe.
+
+    WebPasswordSafe is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    WebPasswordSafe is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebPasswordSafe; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package com.joshdrummond.webpasswordsafe.server;
+
+import org.gwtwidgets.server.spring.gilead.GileadRPCServiceExporter;
+import com.google.gwt.user.client.rpc.SerializationException;
+
+
+/**
+ * Workaround wrapper to guard against CSRF while using GWT-SL RPC exporter
+ * 
+ * @author Josh Drummond
+ *
+ */
+public class CSRFProtectedGileadRPCServiceExporter extends GileadRPCServiceExporter
+{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String processCall(String payload) throws SerializationException
+    {
+        checkPermutationStrongName();
+        return super.processCall(payload);
+    }
+}
