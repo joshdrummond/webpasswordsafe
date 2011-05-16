d6e3532726f6d788bc6c00ccc01d406c3414cdfb
joshdrummond@gmail.com <joshdrummond@gmail.com@acd0af13-0249-0410-9cdc-eb9f26e92fd5>
2011-05-16 10:08:41 +0000

    Updated all 3rd party dependencies to latest stable versions - GWT, GXT, GWT-SL, Hibernate, Spring Framework, Jasypt, Gilead, Apache Commons, Jasper Reports, etc

--- trunk/src/main/java/com/joshdrummond/webpasswordsafe/common/util/Constants.java
+++ trunk/src/main/java/com/joshdrummond/webpasswordsafe/common/util/Constants.java
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009-2010 Josh Drummond
+    Copyright 2009-2011 Josh Drummond
 
     This file is part of WebPasswordSafe.
 
@@ -27,7 +27,7 @@ package com.joshdrummond.webpasswordsafe.common.util;
  */
 public class Constants
 {
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String ADMIN_USER_NAME = "admin";
     public static final String EVERYONE_GROUP_NAME = "Everyone";
     public static final String SESSION_KEY_USERNAME = "X-WPS-Username";
