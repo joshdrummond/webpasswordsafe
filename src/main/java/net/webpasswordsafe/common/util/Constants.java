211ef66ab3a546e7d48a9840ff75a8b9b8fb0a0b
Josh Drummond <joshdrummond@users.noreply.github.com>
2012-06-05 06:31:02 +0000

    GXT3 / ZeroClipboard experimentation

--- src/main/java/net/webpasswordsafe/common/util/Constants.java
+++ src/main/java/net/webpasswordsafe/common/util/Constants.java
@@ -29,7 +29,7 @@ package net.webpasswordsafe.common.util;
 public class Constants
 {
     // versioning
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.3";
     public static final String COPYRIGHT = "2008-2012 Josh Drummond";
     public static final String URL_WEBPASSWORDSAFE = "http://www.webpasswordsafe.net";
     public static final String URL_HELP = "http://code.google.com/p/webpasswordsafe/w/list?q=label:help";
